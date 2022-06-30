68686d8fe16f54764f8912f660686df13ddb5f56
Keshav Jindal <keshav.j@KESHAV-583-GTG.local>
2022-06-30 11:38:13 +0530

    frontend done except pagenation

--- src/components/Inventory.jsx
+++ src/components/Inventory.jsx
@@ -1,20 +1,63 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import '../App.css';
 
 const Inventory = () => {
+  
+  const [wallet , setWallet] = useState([])
+  const [btcUsdtPrice , setbtcUsdtPrice] = useState(0) 
+
+  var btcStream = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@ticker")
+  btcStream.onmessage = function(event){
+    // console.log(JSON.parse(event.data)["c"])
+    setbtcUsdtPrice(JSON.parse(event.data)["c"])
+  }
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/getWallet")
+    .then(res => res.data)
+    .then(data => setWallet(data))
+  } , [])
+  
+  
   return (
     <>
    
     <div className='invent-container'>
-        <div className='heading'>
+        {/* <div className='heading'>
              Wallet
-        </div>
-        <div className='btc-box'>
+        </div> */}
+        {/* <div className='btc-box'>
             <h4>BTC : <span>{1}</span></h4>
         </div>
         <div className='usdt-box'>
             <h4>USDT : <span>{10000}</span></h4>
+        </div> */}
+
+<div className='btcusdt-box'>
+            <h4>Real time price</h4>
+            <h4>1 BTC = <span>{btcUsdtPrice}</span> USDT</h4>
+           
         </div>
+
+        {
+          wallet.map(data => {
+            return (
+              <div >
+                
+              <div className='btc-box'>
+            <h4>BTC left : <span>{data.updated_btc_inventory}</span></h4>
+          </div>
+          <div className='btc-box'>
+            <h4>USDT left: <span>{data.updated_usdt_inventory}</span></h4>
+          </div>
+          
+              </div>
+              
+            )
+          })
+        }
+
     </div>
     </>
   )
--- src/components/OrderList.jsx
+++ src/components/OrderList.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import axios from 'axios';
+import { useEffect, useState } from 'react'
 import '../App.css';
 
 const OrderList = () => {
+ 
+  const [orders , setOrders] = useState([])
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/allTransactions")
+    .then(res => res.data)
+    .then(data => setOrders(data))
+  } , [])
+  
   return (
     <>
     <div className='heading'>
@@ -12,42 +23,44 @@ const OrderList = () => {
     <table>
       <thead>
   <tr>
-    <th>Transaction Type</th>
-    <th>Profit/Loss</th>
     <th>Time</th>
+    <th>Order Type</th>
+    <th>Trade Amount</th>
+    <th>Trade Price</th>
+    <th>Trade Value</th>
+    <th>BTC left</th>
+    <th>USDT left</th>
   </tr>
   </thead>
+  
   <tbody>
-  <tr>
-    <td>Purchase</td>
-    <td> Profit </td>
-    <td>1 AM</td>
+  {/* <tr>
+    <td>1656515540</td>
+    <td> Buy </td>
+    <td>0.001</td>
+    <td>19000.8274</td>
+    <td>19.0008274</td>
+    <td>1.01</td>
+    <td>9910</td>
+  </tr> */}
+{
+  orders.map((order) => {
+    return (
+
+      <tr>
+  <td>{order.time_stamp}</td>
+  <td>{order.order_type}</td>
+  <td>{order.trade_amount}</td>
+  <td>{order.trade_price}</td>
+  <td>{order.trade_value}</td>
+  <td>{order.updated_btc_inventory}</td>
+  <td>{order.updated_usdt_inventory}</td>
   </tr>
-  <tr>
-  <td>Purchase</td>
-    <td> Profit </td>
-    <td>1 AM</td>
-  </tr>
-  <tr>
-  <td>Purchase</td>
-    <td> Profit </td>
-    <td>1 AM</td>
-  </tr>
-  <tr>
-  <td>Purchase</td>
-    <td> Profit </td>
-    <td>1 AM</td>
-  </tr>
-  <tr>
-  <td>Purchase</td>
-    <td> Profit </td>
-    <td>1 AM</td>
-  </tr>
-  <tr>
-  <td>Purchase</td>
-    <td> Profit </td>
-    <td>1 AM</td>
-  </tr>
+
+    )
+  })
+}
+
   </tbody>
 </table>
     </div>
