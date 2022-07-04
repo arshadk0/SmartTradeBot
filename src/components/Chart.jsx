4f8c21859a8dc61baec18f74ea14aeb5d522c229
harshit-zeb <harshit.g@zebpay.com>
2022-07-04 12:46:20 +0530

    frontend design changes

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Bar } from 'react-chartjs-2';
+import zoomPlugin from 'chartjs-plugin-zoom';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -10,6 +11,7 @@ import {
   Legend,
 } from 'chart.js'
 import '../App.css';
+import { getOnlyTimeFromUnixTime} from '../utils';
 
 ChartJS.register(
   CategoryScale,
@@ -17,25 +19,40 @@ ChartJS.register(
   BarElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  zoomPlugin
 );
 
-const cdata =[2,3,-4,-6,5];
-const color = cdata.map((item) => item>0 ? 'rgba(53, 162, 235, 0.5)' : 'rgba(255, 99, 132, 0.5)');
 
-const Chart = () => {
-     
+const Chart = ({pwallet}) => {
+
+  const [pldata, setPlData] = useState([]);
+  
+  useEffect(() =>{
+    setPlData([...pldata, {time: pwallet[0]?.time_stamp , value : pwallet[0]?.profit_loss}]);
+  },[pwallet])
+
+
+  const valuedata = pldata?.map((item) => item?.value);
+  const color = valuedata.map((item) => item>0 ? 'rgba(53, 162, 235, 0.5)' : 'rgba(255, 99, 132, 0.5)');
+
+  var timedata = pldata?.map((item) => getOnlyTimeFromUnixTime(item?.time));
+  timedata = timedata.filter(item => item!== 'NaN:aN:aN');
+
+  console.log(timedata);
   const data = {
-    labels: ['Red', 'Orange', 'Blue','green' , 'yellow'],
+    labels: timedata,
     datasets: [
         {
           label: ['Profit'],
-          data: cdata,
+          data: valuedata,
           backgroundColor: color,
           borderWidth: 0.2,
         }
     ]
 }
+
+//console.log("heyyyyyyyyyyyyyyyyy",pldata);
  
   return (
     <>
@@ -54,6 +71,17 @@ const Chart = () => {
             legend: {
               display: true,
               position: "bottom"
+           },
+           zoom :{
+            zoom: {
+              wheel: {
+                enabled: true,
+              },
+              pinch: {
+                enabled: true
+              },
+              mode: 'x',
+            }
            }
           }
         }}
